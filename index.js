0f45e5b957dfaafe5a26737cd1ee0365e604f026
QsunAum <rykyu01@gmail.com>
2019-09-06 00:45:42 +0900

    crawling commit

--- index.js
+++ index.js
@@ -15,6 +15,16 @@ records.forEach((r,i)=>{
     console.log(i, r.제목, r.가격, r.링크);
 });
 
-// const crawler = async()=>{
-//     await Promise.all();
-// };
+const crawler = async()=>{
+    await Promise.all(records.map(async(r)=>{
+        const response = await axios.get(r.링크);
+        if(response.status ===200){
+            const html = response.data;
+            const $ = cheerio.load(html);
+            const point= $('.info_list.Ere_fs15.Ere_ht18 .Ere_sub_pink.Ere_fs16.Ere_str').text();
+            console.log(r.제목, '평점',point.trim());
+        }
+    }));
+};
+
+crawler();
